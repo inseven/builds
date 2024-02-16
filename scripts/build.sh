f2a7a86a854d4fadf31f2694633ddbcb58537fe9
Jason Morley <hello@jbmorley.co.uk>
2024-02-15 18:07:24 -1000

    Try running the clean and build steps separately *shrug*

--- scripts/build.sh
+++ scripts/build.sh
@@ -134,7 +134,17 @@ xcodebuild \
     OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" \
     BUILD_NUMBER=$BUILD_NUMBER \
     MARKETING_VERSION=$VERSION_NUMBER \
-    clean archive
+    clean
+xcodebuild \
+    -project Builds.xcodeproj \
+    -scheme "Builds" \
+    -config Release \
+    -destination "platform=iOS,arch=arm64" \
+    -archivePath "$IOS_ARCHIVE_PATH" \
+    OTHER_CODE_SIGN_FLAGS="--keychain=\"${KEYCHAIN_PATH}\"" \
+    BUILD_NUMBER=$BUILD_NUMBER \
+    MARKETING_VERSION=$VERSION_NUMBER \
+    archive
 xcodebuild \
     -archivePath "$IOS_ARCHIVE_PATH" \
     -exportArchive \
