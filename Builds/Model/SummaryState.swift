8f95023ed540f8ff7866188d500feba45afd2d5c
Jason Morley <hello@jbmorley.co.uk>
2024-03-10 12:52:07 -1000

    refactor: Extract summary logic into common `SummaryState` constructor

--- Builds/Model/SummaryState.swift
+++ Builds/Model/SummaryState.swift
@@ -27,4 +27,56 @@ enum SummaryState {
     case failure
     case inProgress
 
+    init(status: GitHub.Status?, conclusion: GitHub.Conclusion?) {
+        guard let status else {
+            self = .unknown
+            return
+        }
+
+        switch status {
+        case .queued:
+            self = .inProgress
+            return
+        case .waiting:
+            self = .inProgress
+            return
+        case .inProgress:
+            self = .inProgress
+            return
+        case .completed:
+            break
+        }
+
+        guard let conclusion else {
+            self = .unknown
+            return
+        }
+
+        switch conclusion {
+        case .success:
+            self = .success
+            return
+        case .failure:
+            self = .failure
+            return
+        case .cancelled:
+            self = .failure
+            return
+        }
+
+    }
+
+    var color: Color {
+        switch self {
+        case .unknown:
+            return .gray
+        case .success:
+            return .green
+        case .failure:
+            return .red
+        case .inProgress:
+            return .yellow
+        }
+    }
+
 }
