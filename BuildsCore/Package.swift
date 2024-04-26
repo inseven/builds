40ed5a1ec720aa81aa20392d40adb0a7f154e2b9
Jason Morley <hello@jbmorley.co.uk>
2024-04-26 09:35:58 -1000

    refactor: Remove redundant comments from Package.swift (#386)

--- BuildsCore/Package.swift
+++ BuildsCore/Package.swift
@@ -11,7 +11,6 @@ let package = Package(
         .macOS(.v14),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BuildsCore",
             targets: ["BuildsCore"]),
@@ -20,8 +19,6 @@ let package = Package(
         .package(path: "../interact"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "BuildsCore",
             dependencies: [
